47a18015448a74df558cb9bd9869432682029c7f
sisrecords <sisrecords@gmail.com>
2024-04-24 11:09:07 +0300

    Initial commit

---
+++ ios/Pods/Target Support Files/Pods-ManitouApp24-ManitouApp24Tests/Pods-ManitouApp24-ManitouApp24Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ManitouApp24_ManitouApp24Tests : NSObject
+@end
+@implementation PodsDummy_Pods_ManitouApp24_ManitouApp24Tests
+@end
---
+++ ios/Pods/Target Support Files/Pods-ManitouApp24/Pods-ManitouApp24-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ManitouApp24 : NSObject
+@end
+@implementation PodsDummy_Pods_ManitouApp24
+@end
